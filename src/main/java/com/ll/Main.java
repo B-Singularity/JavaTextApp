2410cb2b81497ff1ab78ff986541db3eafb3e555
B-singularity <skil1489@naver.com>
2023-10-25 14:27:28 +0900

    세팅

---
+++ src/main/java/com/ll/Main.java
@@ -0,0 +1,12 @@
+package com.ll;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        WiseSaying saying = new WiseSaying();
+        System.out.println("== 명언 앱 ==");
+        saying.start();
+
+    }
+}
+
---
+++ src/main/java/com/ll/WiseSaying.java
@@ -0,0 +1,28 @@
+package com.ll;
+import java.util.Scanner;
+
+public class WiseSaying {
+    String command;
+    String artist;
+    Scanner scanner;
+    WisdomRegistry wisdomRegistry;
+
+    public WiseSaying() {
+        scanner = new Scanner(System.in);
+        wisdomRegistry = new WisdomRegistry();
+    }
+
+    void start() {
+        while (true) {
+            System.out.printf("명령) ");
+            this.command = scanner.nextLine();
+            if (command.equalsIgnoreCase("등록")) {
+                wisdomRegistry.registerWiseSaying();
+            } else if (command.equalsIgnoreCase("종료")) {
+                break;
+            } else if (command.equalsIgnoreCase("목록")) {
+               WisdomShow.show();
+            }
+        }
+    }
+}
